5f7612e8282236634b1d7db1711430e6ea6f3349
taiuanasc04 <taiua.nascimento@outlook.com>
2022-02-15 20:44:01 -0300

    Adicionando linha impressa 17

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,14 +5,20 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         int primeiroNumero = 5;
         int segundoNumero = 3;
-        String operacao = "-";
+        String operacao = "+";
         System.out.println("Calculadora");
         System.out.println();
         if (operacao == "+"){
             System.out.println(primeiroNumero + " + " + segundoNumero + " = " + soma(primeiroNumero, segundoNumero));
-        } if (operacao == "-"){
-            System.out.println(primeiroNumero + " - " + segundoNumero + " = " + subtracao(primeiroNumero, segundoNumero));
+        } else {
+            if (operacao == "-"){
+                System.out.println(primeiroNumero + " - " + segundoNumero + " = " + subtracao(primeiroNumero, segundoNumero));
+            } else {
+                System.out.println("Nenhuma operaćão valida foi selecionada.");
+            }
         }
+
+        System.out.println("Este é um testa para calculadora..");
 
     }
 
